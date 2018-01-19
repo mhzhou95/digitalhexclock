7ead5837661de7631274c0709b556e78952b2eec
unknown <minghaozhou123@gmail.com>
2018-01-19 14:39:11 -0500

    final commit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-setInterval(function(){
-  var d = new Date();
-  var h = d.getHours();
-  var m = d.getMinutes();
-  var s = d.getSeconds();
-  if (h>10) {
-    hours.innerHTML = h
-  }else {
-    hours.innerHTML = "0" + h
-  }
-  if (m>10) {
-    minutes.innerHTML = m
-  }else {
-  minutes.innerHTML = "0" + m
-  }
-  if (s>10) {
-    seconds.innerHTML = s
-  }else {
-      seconds.innerHTML = "0" + s
-  }
+setInterval(function() {
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    if (h > 9) {
+        hours.innerHTML = h
+    } else {
+        hours.innerHTML = "0" + h
+    }
+    if (m > 9) {
+        minutes.innerHTML = m
+    } else {
+        minutes.innerHTML = "0" + m
+    }
+    if (s > 9) {
+        seconds.innerHTML = s
+    } else {
+        seconds.innerHTML = "0" + s
+    }
 }, 1000);
 
 setInterval(function() {
-  var hex = hours.innerHTML + minutes.innerHTML + seconds.innerHTML;
-  document.getElementById('body').style.backgroundColor = "#" + hex;
+    var hex = hours.innerHTML + minutes.innerHTML + seconds.innerHTML;
+    document.getElementById('body').style.backgroundColor = "#" + hex;
 }, 2000)
